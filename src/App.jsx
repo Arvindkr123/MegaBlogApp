32d793563765c5f58ad3ba1115db5c321b3eaf0e
Arvind Kumar <thakurarvindkr10@gmail.com>
2023-09-27 06:06:50 +0530

    added database service

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,37 @@
-import "./App.css";
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Footer, Header } from "./Components";
 
 function App() {
-  //console.log(import.meta.env.VITE_APP_APPWRITE_URL);
-  return <div>A Blog in App with appwrite</div>;
+  const [loading, setLoading] = useState(false);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally((error) => {
+        setLoading(false);
+      });
+  }, []);
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap items-center justify-between bg-gray-400">
+      <div className="w-full block text-center">
+        <Header />
+        <main></main>
+        <Footer />
+      </div>
+    </div>
+  ) : (
+    <div>Loading.....</div>
+  );
 }
 
 export default App;
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => {
+  return <div className="text-2xl">This is a footer Components</div>;
+};
+
+export default Footer;
