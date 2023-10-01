9443cfff598ecf6295b794e7d1cd31ce111fb411
Arvind Kumar <thakurarvindkr10@gmail.com>
2023-10-01 12:49:56 +0530

    added

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,71 @@ import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./store/store.js";
-import { BrowserRouter } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Home, AddPost, AllPost, EditPost, Login, Post, SignUp } from "./pages";
+import { AuthLayOut } from "./Components";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: (
+          <AuthLayOut authentication={false}>
+            <Login />
+          </AuthLayOut>
+        ),
+      },
+      {
+        path: "/signup",
+        element: (
+          <AuthLayOut authentication={false}>
+            <SignUp />
+          </AuthLayOut>
+        ),
+      },
+      {
+        path: "/all-posts",
+        element: (
+          <AuthLayOut authentication>
+            <AllPost />
+          </AuthLayOut>
+        ),
+      },
+      {
+        path: "/add-post",
+        element: (
+          <AuthLayOut authentication>
+            <AddPost />
+          </AuthLayOut>
+        ),
+      },
+      {
+        path: "/edit-post/:slug",
+        element: (
+          <AuthLayOut>
+            <EditPost />
+          </AuthLayOut>
+        ),
+      },
+      {
+        path: "/post/:slug",
+        element: <Post />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import Home from "./Home";
+import AddPost from "./AddPost";
+import EditPost from "./EditPost";
+import AllPost from "./AllPost";
+import Login from "./Login";
+import Post from "./Post";
+import SignUp from "./SignUp";
+
+export { Home, AddPost, EditPost, Login, AllPost, Post, SignUp };
